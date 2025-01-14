91534a52088fa5ed907ffbc09e4e3cb522359122
DoheumPark <pirbak@hanmail.net>
2025-01-14 13:06:48 +0900

    스트림 + 람다식 이해

--- src/main/java/com/green/jwt/config/jwt/JwtUser.java
+++ src/main/java/com/green/jwt/config/jwt/JwtUser.java
@@ -10,6 +10,14 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.function.Function;
+
+class MyFunction implements Function<String, SimpleGrantedAuthority> {
+    @Override
+    public SimpleGrantedAuthority apply(String str) {
+        return new SimpleGrantedAuthority(str);
+    }
+}
 
 @Setter
 @Getter
@@ -18,6 +26,11 @@ public class JwtUser implements UserDetails {
     private long signedUserId;
     private List<String> roles; //인가(권한)처리 때 사용, ROLE_이름, ROLE_USER, ROLE_ADMIN
 
+    //리턴타입으로 콜렉션인데 콜렉션에 방 하나하나의 타입은 <> 지정을 한다.
+    //<?>이렇게 하면 Object가 되기 때문에 모든 타입이 허용이 된다.
+    //<? extends GrantedAuthority>는 지정 타입이 꼭 GrantedAuthority포함, GrantedAuthority를 상속받은 객체만 가능하도록 제한을 거는 것
+
+    //<? super GrantedAuthority>는 지정 타입이 꼭  GrantedAuthority포함, GrantedAuthority의 부모 객체만 가능하도록 제한을 거는 것
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
 //        List<GrantedAuthority> authorities = new ArrayList<>(roles.size());
@@ -26,10 +39,21 @@ public class JwtUser implements UserDetails {
 //        }
 //        return authorities;
 
+        Function fn2 = new MyFunction();
+
+        Function fn = new Function<String, SimpleGrantedAuthority>() {
+            @Override
+            public SimpleGrantedAuthority apply(String str) {
+                return new SimpleGrantedAuthority(str);
+            }
+        };
         return roles.stream() //List<String>을 Stream<List<String>>으로 변환
                      // SimpleGrantedAuthority::new은 메소드 참조라고 호칭
-                     // .map(item -> new SimpleGrantedAuthority(item))와 같은 내용이다.
-                    .map(SimpleGrantedAuthority::new) //map은 똑같은 size의 스트림을 만든다. Stream<List<SimpleGrantedAuthority>>으로 변환,
+                    //.map(fn)
+                    // .map(item -> new SimpleGrantedAuthority(item))와 같은 내용이다.
+                    // .map(item -> { return new SimpleGrantedAuthority(item); }) //와 같은 내용이다.
+                .map(fn2)
+                //.map(SimpleGrantedAuthority::new) //map은 똑같은 size의 스트림을 만든다. Stream<List<SimpleGrantedAuthority>>으로 변환,
                     .toList();
     }
 
